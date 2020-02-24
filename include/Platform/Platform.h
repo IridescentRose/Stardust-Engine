7531b6d5b8f16959b6f27d70d1c4e277000027db
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-02-24 00:24:01 -0500

    Platform codes

---
+++ include/Platform/Platform.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define PLATFORM_PSP 0
+#define PLATFORM_PC 1
+#define PLATFORM_OTHER 2
+
+//Switch this line of code for other build support
+#define CURRENT_PLATFORM PLATFORM_PSP
+
+namespace Stardust::Platform {
+#ifndef CURRENT_PLATFORM
+#error No Platform was Defined!
+#endif
+
+	/*
+	Init Platform performs all needed platform initialization code
+	*/
+	void initPlatform();
+
+	/*
+	Exit Platform performs all needed platform exit code (this is for forced exits)
+	*/
+	void exitPlatform();
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,24 @@
 #include <pspkernel.h>
 #include <pspdebug.h>
-#include <Utilities/Common.h>
+#include <Platform/Platform.h>
+
+#if CURRENT_PLATFORM == PLATFORM_PSP
 
 PSP_MODULE_INFO("Stardust", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);
 PSP_HEAP_SIZE_KB(-1024);
 
+#endif
+
+using namespace Stardust;
+
 int main() {
-	pspDebugScreenInit();
-	SetupCallbacks();
-	pspDebugScreenPrintf("Hello World!");
-	sceKernelDelayThread(1000 * 1000 * 3);
-	sceKernelExitGame();
+
+	Platform::initPlatform();
+
+	
+
+	Platform::exitPlatform();
+
 	return 0;
 }
