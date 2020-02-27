ef3f7d860c18f389918519d6133afd9671f7bfe3
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-02-27 05:46:45 -0500

    Ray Casts & AABB
    Former-commit-id: dace8a8237a4e4deb0f530cae2ef16393eecc2d1

---
+++ include/Math/Ray.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Math/AABB.h>
+
+namespace Stardust::Math {
+
+	class Ray2D {
+	public:
+		Ray2D(vec2 position, vec2 direction): m_pos(position), m_dir(direction) {
+			m_newPos = m_pos;
+		}
+
+		//Number of steps
+		inline void step(float multiplier) {
+			m_newPos += m_dir * multiplier;
+		}
+
+		inline vec2 getEnd() {
+			return m_newPos;
+		}
+
+	private:
+		vec2 m_pos, m_dir;
+		vec2 m_newPos;
+
+	};
+}
