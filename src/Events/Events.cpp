98508d92f12fab84b4c742cc75fc9ac4c780c846
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-03-05 23:47:12 -0500

    Event listeners

--- src/Events/Events.cpp
+++ src/Events/Events.cpp
@@ -4,6 +4,7 @@ namespace Stardust::Events {
 	EventBus::EventBus()
 	{
 		eventList.clear();
+		eventListeners.clear();
 	}
 	EventBus::~EventBus()
 	{
@@ -18,6 +19,28 @@ namespace Stardust::Events {
 		for (int i = 0; i < eventList.size(); i++) {
 			delete eventList.at(i);
 		}
+		eventList.clear();
+	}
+	void EventBus::addEventHandler(int e_type, EventHandler h)
+	{
+		eventListeners.emplace(e_type, h);
+	}
+	void EventBus::deleteListener(int e)
+	{
+		eventListeners.erase(eventListeners.find(e));
+	}
+	void EventBus::clearEventHandlers()
+	{
+		eventListeners.clear();
+	}
+	void EventBus::propagate()
+	{
+		for (int i = 0; i < eventList.size(); i++) {
+			if (eventListeners.find(eventList[i]->event_type) != eventListeners.end()) {
+				eventListeners[eventList[i]->event_type](eventList[i]);
+			}
+		}
+
 		eventList.clear();
 	}
 }
