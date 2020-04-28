b0231ef7b89f9867320fd0b804e11ceddcfd47c5
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-04-28 01:56:21 -0400

    Animated tile maps :smiley:
    Former-commit-id: f750b92fa3c4953918182c605f51edd243fde6e8 [formerly 7d4c2683898aafabadf2a482442878de0ad897f5 [formerly a7ad9f618287824ad43861538bd6c3a466574336 [formerly 9ad13e8cccb21c19c7ed707ef12595fafc7ff3ec [formerly ebc226161187a053e74bc43173537d77241b71ed]]]]
    Former-commit-id: 93329ce6795d08ad5ef3e1729761ba8b14255345
    Former-commit-id: ad9e4cd5f878d671aac299187e6dfc38e9d0bdd9
    Former-commit-id: 859ae2a165c7f347e17eff05262193942c244cd1
    Former-commit-id: c2ac57b1e7baa1f954cce993b6468169e88fa948

---
+++ include/Graphics/2D/TilemapAnim.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "SpriteAdvanced.h"
+#include "Tilemap.h"
+
+namespace Stardust::Graphics::Render2D {
+
+	struct TileAnim : public Tile {
+		bool isAnim;
+		short indexStart;
+		char animLength;
+		char tickNumber;
+	};
+
+	class TilemapAnim {
+	public:
+		TilemapAnim(TextureAtlas* atlas, Texture* texture);
+
+		int addTile(TileAnim* t);
+		void updateTile(int tileNum, TileAnim* t);
+		void tickAnimation();
+		void clearTiles();
+
+		void buildMap();
+
+		void drawMap();
+
+	private:
+		TextureAtlas* m_Atlas;
+		std::vector<TileAnim*> m_Tiles;
+		std::vector<Vertex> vertices;
+		Texture* texRef;
+
+
+	};
+
+}
