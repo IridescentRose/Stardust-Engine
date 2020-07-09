f011b8cc15106d36c28ddee7dbe33cc0f6992803
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-07-09 15:46:03 -0400

    Include sfml, cross plat audio, include glad
    Former-commit-id: 1b0a8ead67cc90a1007d57af4032f63bcb547ad4 [formerly e1c76d9aed1b9a6820e320ec61fb9d28f449b685 [formerly f755478ce9650c4d1799c86d95609f9d86179a2f [formerly 5be0fe8b569355582b1ac61f560e465cc286c1f4 [formerly b73078f377350f1ac01688085895410994d467d4]]]]
    Former-commit-id: e228607f19972e16cfcc9e3c6f176e5a19070897
    Former-commit-id: 787e3827fafc2412913cba7855a1930991c02d9a
    Former-commit-id: 6883851b029a4f702cb8c02d39940d916fd4d755
    Former-commit-id: ac0e4e13892dcdc102d9a1ba59eeab905a71a85d

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,16 +1,20 @@
 #include <Platform/Platform.h>
 #include <Platform/PC/Window.h>
+#include <Audio/AudioClip.h>
 
 using namespace Stardust;
 
 int main() {
+	Platform::initPC();
 	Platform::initPlatform();
-	Platform::PC::Window* w = new Platform::PC::Window(800, 640, "test", false, false);
 
-	while (!w->shouldClose()) {
+	Audio::AudioClip* audio = new Audio::AudioClip("runereality.ogg");
+	audio->Play();
+
+	while (!Platform::PC::g_Window->shouldClose()) {
 		//Main loop
-		w->update();
-		w->draw();
+		Platform::PC::g_Window->update();
+		Platform::PC::g_Window->draw();
 	}
 
 	Platform::exitPlatform();
