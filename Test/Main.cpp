2c6496342de1ada28be1ccc8d69e885537f8edc0
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-07-10 06:30:59 -0400

    Begin graphics reworks.
    Former-commit-id: 1276d5c87169aeb0b4a2dc9c89f5eaa5e67b7484 [formerly fd0f6c21e69a02241106c31dbb5fc709a5bd4ddb [formerly e26b20ec2e331a085c37432d68672e0be70981e1 [formerly e704e0983418cf3b793a8ea52c7a877e1cb1662a [formerly 300f36a43baa0811c6aa287258f2530bd0378895]]]]
    Former-commit-id: 6f52583cc0d498de5d83e9411b8d818d3b527617
    Former-commit-id: 8fabcba13d3c3e543725b7610ff6ad32c4612182
    Former-commit-id: d2aef661aca86e5b373465e9dfa69e1ff9944a4c
    Former-commit-id: ad521843b48790911839603959f110ef41cc61b4

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -10,10 +10,7 @@ int main() {
 
 	float r = 0;
 	while (!Platform::PC::g_Window->shouldClose()) {
-		r += 0.0001f;
-		if (r > 1.0f) {
-			r = 0.0f;
-		}
+		
 		//Main loop
 		Platform::PC::g_Window->update();
 		Platform::PC::g_Window->draw();
