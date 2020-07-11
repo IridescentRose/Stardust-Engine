3f7094d1c8996437264adba9d7f23379970005d7
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-07-11 01:45:45 -0400

    Graphics wrapper
    Former-commit-id: 4e6f1e5e7b705659fecd1927880173bae6b6ba04 [formerly cdf18af964c7cae1627f728a5e9b7bfcba344275 [formerly 74a3cc94f4257d76f650dee78e64b9eeeb807fc3 [formerly 424abcb7fbb7462c269c673927157ae443b869a2 [formerly 0807a2e7845a02f75ac651bac5936d95c7686c94]]]]
    Former-commit-id: e4ed1abca92fee25b8801709bd2944a3fb5cbaf1
    Former-commit-id: fc74a09c38f11178ce4df2abf27bf3845d088a08
    Former-commit-id: da3cdaef5bf9f72bfd41901ef70e20126d4cfda7
    Former-commit-id: 51eba1e5acdfee975f4970cb85589654f7369b58

---
+++ Core/include/GFX/RenderCore.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <Platform/Platform.h>
+#include <Platform/Graphics.h>
+
+namespace Stardust::GFX {
+	class RenderCore {
+	public:
+		RenderCore();
+		~RenderCore();
+
+		void init();
+		void cleanup();
+
+		void clear();
+		void setClearColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+		void setClearColor(float r, float g, float b, float a);
+
+		void set2DMode(float bottom, float top, float left, float right, float znear, float zfar);
+
+		void beginFrame();
+		void endFrame();
+
+	private:
+		uint8_t r, g, b, a;
+	};
+
+	extern RenderCore* g_RenderCore;
+
+}
