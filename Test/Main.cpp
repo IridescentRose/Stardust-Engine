48418f8fdba24f2406b18e1c3271da4028c3f219
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-07-16 22:10:29 -0400

    Added animated stuffs
    Former-commit-id: 430f2676795626b2ee7ee4a2f1ce7e2ae976c98e [formerly fc248262e8eacef149dd8711eb2422fe814556e6 [formerly 97c50505c378cebaba756db8839c5a48311ff0d3 [formerly 4677482f638a607cf2f04c56feaeebd9cd611585 [formerly 720fdf71ad6c37bd5017160e43cf7be312c85253]]]]
    Former-commit-id: e9604bf118dae691b6aab816e8a9f25f7c3d7116
    Former-commit-id: e2612d3323f18337cbecee3e6be3785f48938268
    Former-commit-id: e81ee2b8524e834a2c1d479d9526edbb96c9059e
    Former-commit-id: 4178868cd65b0aaa07013e1644aa4064abbfd8c7

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -2,32 +2,32 @@
 #include <GFX/RenderCore.h>
 #include <Platform/PC/Window.h>
 #include <GFX/GFXWrapper.h>
-#include <GFX/2D/Sprite.h>
+#include <GFX/2D/SpriteAnimated.h>
 using namespace Stardust;
 
 int main() {
 	Platform::initPlatform();
 
-	GFX::g_RenderCore->set2DMode(-16.0f / 9.0f, 16.0f / 9.0f, -1.0f, 1.0f, -10.0f, 10.0f);
-
 	float r = 0.0f;
 
-	unsigned int myTex = GFX::g_TextureManager->loadTex("test.png", GFX_FILTER_LINEAR, GFX_FILTER_LINEAR, true);
-	GFX::Render2D::Sprite* sprite = new GFX::Render2D::Sprite(myTex, { 0.5f, 0.5f });
+	unsigned int myTex = GFX::g_TextureManager->loadTex("fire_0.png", GFX_FILTER_NEAREST, GFX_FILTER_NEAREST, true);
+	GFX::Render2D::SpriteAnimated* sprite = new GFX::Render2D::SpriteAnimated(myTex, { 256, 256 }, 32);
+
+	sprite->setPosition(240, 136);
 
 	while (true) {
 		GFX::g_RenderCore->beginFrame();
 
-		r += 0.01f;
+		r += 0.005f;
 		if (r >= 1.0f) {
 			r = 0.0f;
+			sprite->tickPhase();
 		}
 
-		GFX::g_RenderCore->setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		GFX::g_RenderCore->setClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		GFX::g_RenderCore->clear();
 
 		//Main loop
-		sprite->setColor(1.0f * r, 0.0f, (1.0f-r) * 1.0f, 1.0f);
 		sprite->draw();
 
 		Platform::platformUpdate();
