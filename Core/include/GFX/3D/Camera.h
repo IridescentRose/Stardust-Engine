9c21d72711df82501372122b8dac7516ff3eba94
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-07-18 03:09:06 -0400

    Fixed skybox & camera issues.
    Former-commit-id: e5e5c901602c0b9d011eedc716bfcfd1c9804b32 [formerly 6612a7cec1c26e628355345c7dbda24516ad133a]
    Former-commit-id: 59ddd0dc87161cc31ad61eb00b02bd427df4fc18
    Former-commit-id: d408cfdfe8033a5e13b09e54b590f4259163db86
    Former-commit-id: a4109abdc86a8e084bfca3efbfaa4376acf47e70
    Former-commit-id: 08162b0cbd723f7fe61bb3616cfb41aa3f76abcd

--- Core/include/GFX/3D/Camera.h
+++ Core/include/GFX/3D/Camera.h
@@ -1,5 +1,8 @@
 #pragma once
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
+#include <GFX/GFXWrapper.h>
+#include <Platform/Platform.h>
 
 namespace Stardust::GFX::Render3D {
 	class Camera {
@@ -9,12 +12,22 @@ namespace Stardust::GFX::Render3D {
 		void setProjection(float fovy, float aspect, float zN, float zF);
 		void update();
 
+#if CURRENT_PLATFORM == PLATFORM_PSP
+		inline ScePspFMatrix4 getProjection() {
+			return proj;
+		}
+		inline ScePspFMatrix4 getView() {
+			return view;
+		}
+#else
 		inline glm::mat4 getProjection() {
 			return proj;
 		}
 		inline glm::mat4 getView() {
 			return view;
 		}
+#endif
+		
 
 		glm::vec3 pos;
 		glm::vec3 rot;
@@ -22,6 +35,10 @@ namespace Stardust::GFX::Render3D {
 
 		float fov, aspectRatio, zNear, zFar;
 
+#if CURRENT_PLATFORM == PLATFORM_PSP
+		ScePspFMatrix4 proj, view;
+#else
 		glm::mat4 proj, view;
+#endif
 	};
 }
